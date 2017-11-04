c1fbe74008429b9f235105893be86e430462f84f
teruya <abas.trace.on@gmail.com>
2017-11-04 16:15:32 +0900

    add rpimac

--- sendtodb.py
+++ sendtodb.py
@@ -5,23 +5,26 @@ import sys
 import traceback
 import math
 import sqlite3
-from enum import Enum
+import uuid
 
 MAC = 0
 INI = 1
 FIN = 2
 PWR = 3 
 DATA = 4
-ESSID = 5
+BSSID = 5
 DIST = 7
+RPI_MAC = 8
 
+MINCUL = 6
 
 def create_table(conn, cur):
     cur.execute("""CREATE TABLE distance (
         id         INTEGER PRIMARY KEY AUTOINCREMENT,
         macaddr    TEXT,
         pwr        INTEGER,
-        distance   INTEGER
+        distance   INTEGER,
+        rpimac     TEXT
         )""")
     conn.commit()
 
@@ -44,8 +47,8 @@ def isexist(conn, cur):
 
 def insert_data(conn, cur, data):
     cur.execute("""
-        INSERT INTO distance VALUES(NULL,?,?,?)
-        """, (data[MAC], data[PWR], data[DIST],))
+        INSERT INTO distance VALUES(NULL,?,?,?,?)
+        """, (data[MAC], data[PWR], data[DIST], data[RPI_MAC]))
     conn.commit()
 
 
@@ -55,7 +58,8 @@ def get_distance(pwr):
 
 def main(argv):
     filename = argv[0]
-    essid = argv[1]
+    essid = argv[1] #可読なAPの識別子
+    bssid = '' #APのMACaddr
     interval = float(argv[2])
     dbname = 'distance.db'
     (conn, cur) = getdb(dbname)
@@ -67,14 +71,24 @@ def main(argv):
             with open(filename, 'r', newline='') as fin:
                 reader = csv.reader(fin)
                 header = next(reader)
+                # APのBSSIDを特定
                 for row in reader:
-                    if len(row) > ESSID and row[ESSID].lstrip() == essid:
+                    if row[-1].lstrip() == essid:
+                        bssid = row[0].lstrip()
+                        break
+
+                for row in reader:
+                    if len(row) >= MINCUL and\
+                                    row[BSSID].lstrip() == bssid and\
                         # 測定不能なホストを飛ばす
                         if int(row[PWR]) == -1:
                             continue
                         distance = get_distance(row[PWR])
+                        rpi_mac = hex(uuid.getnode())[2:]
                         row.append(distance)
+                        row.append(rpi_mac)
                         insert_data(conn, cur, row)
+
             time.sleep(interval)
 
         except KeyboardInterrupt:
